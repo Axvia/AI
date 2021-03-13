9a719a47dc10ca2f2d9fdcfc92c1357c60711a5b
Axvia <65358975+Axvia@users.noreply.github.com>
2021-03-13 09:27:07 +0800

    Last one

--- index.js
+++ index.js
@@ -17,9 +17,8 @@ async function Index() {
     const { Client, Intents, Collection, MessageEmbed, MessageAttachment } = require('discord.js')
     const client = new Client({disableMentions: 'everyone', ws: { intents: Intents.ALL }});
     const { blueBright, greenBright, redBright, grey, yellowBright } = require('chalk')
-
+    
     // ########################### COMMAND MODULES ###################################### //
-    const fs = require('fs')
     const cooldowns = new Collection();
     client.commands = new Collection();
     client.aliases = new Collection();
@@ -27,34 +26,26 @@ async function Index() {
     handlers(client)
 
     // ############################ CUSTOM FUNCTIONS ###################################### //
-    const exCommand = async function exCommand(data, category, client, message) { 
-        return require(`./commands/${category}/${data}.js`)(client, message) 
+    const exCommand = async function exCommand(data, category, path, client, message) { 
+        return require(`${path}commands/${category}/${data}.js`)(client, message) 
     }
 
     // ############################ CUSTOM MODULES ###################################### //
-    const { Console, channelID, Command, Send, DMchannel, hasID, hasRole, timeAgo } = require('./modules/utility')
+    const { Console, channelID, Command, DMchannel, hasID, timeAgo } = require('./modules/utility')
     const ai = new AI(client)
 
-    // ############################# MESSAGING ####################################### //
+    // ############################# General Messaging ####################################### //
     client.on('message', async(message)=>{
         if (message.author.bot) return;
         if (await DMchannel(message)) return false;
+        // Arguments input
+        const args = message.content.slice(ai.prefix.length).trim().split(/ +/);
         if (await channelID(message, "806139190164717568")) {
-            if (await  Command(message, 'brawlhalla', { prefix: ai.prefix })) {
-                await exCommand("brawlhalla", "software", client, message).catch(() => {})
+            if (await  Command(message, 'brawlhalla', { prefix: "!" })) {
+                await exCommand("brawlhalla", "software", './', client, message).catch(() => {})
             }
         }
-        const args = message.content.slice(ai.prefix.length).trim().split(/ +/);
         if (await hasID(message, '802906117318770688') || await hasID(message, '714486020594204754')) {
-            if (await Command(message, 'args', { prefix: ai.prefix })) {
-                message.channel.send(`- Args 1: '${args[1] || 'Null'}'\n- Args 2: '${args[2] || 'Null'}'\n- Args 3: '${args[3] || 'Null'}'\n- Args 4: '${args[4] || 'Null'}'\n- Args 5: '${args[5] || 'Null'}'`, { code: 'fix' })
-            }
-            if (await Command(message, 'password', { prefix: ai.prefix })) {
-                await message.channel.send(`${await Mod.Password(16) || "I broke the code"}`)
-            }
-            if (await Command(message, "foo", { prefix: ai.prefix })) {
-                await Send(message, "bar", false, { emoji: undefined })
-            }
             if (await Command(message, "loyalty", { prefix: ai.prefix })) {
                 if (!args[1]) {
                     await message.channel.send(timeAgo(Date.parse(message.member.joinedAt)))
@@ -64,65 +55,33 @@ async function Index() {
                     await message.channel.send(timeAgo(Date.parse(user.joinedAt)))
                 }
             }
-            if (await Command(message, 'time format', {prefix : ai.prefix})) {
-                const today = new Date(Date.now())
-                const content = [
-                    `1. Locale Date String: ${today.toLocaleDateString()}\n`+
-                    `2. Locate Time String: ${today.toLocaleTimeString()}\n`+
-                    `3. Locate String: ${today.toLocaleString()}\n`+
-                    `4. Date String: ${today.toDateString()}\n`+
-                    `5. ISO String: ${today.toISOString()}\n`+
-                    `6. String: ${today.toString()}\n`+
-                    `7. Time String: ${today.toTimeString()}\n`+
-                    `8. UTC String: ${today.toUTCString()}`
-                ]
-                const embed = new MessageEmbed().setColor("GOLD")
-                .setDescription(`\`\`\`ps` + '\n' + content + `\n` + `\`\`\``)
-                .setFooter(`${client.user.tag} • ${today.toString()}`, client.user.displayAvatarURL())
-                await message.channel.send(embed)
-            }
-            if (await Command(message, 'purge', { prefix: ai.prefix })) {
-                if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.channel.send(`Missing Permission to \`Manage Messages\``) 
-                const Options = args[1];
-                const Amount = args[2];
-                if (Options === 'text') {
-                    if (!Amount) return message.channel.send('Please provide a numeric input!');
-                    if (isNaN(Amount)) return message.channel.send('Please provide a valid numeric input!');
-                    if (Amount > 100) return message.channel.send('Uh oh, the amout you provided too high.');
-                    const fetchChannel = await message.channel.messages.fetch({limit:Amount})
-                    fetchChannel.filter(msg => {if (typeof msg.content === 'string') return msg.content}).map(msg => msg.delete().catch(()=>{}))
-                } else if (Options === 'old') {
-                    if (!Amount) return message.channel.send('Please provide a numeric input!');
-                    if (isNaN(Amount)) return message.channel.send('Please provide a valid numeric input!');
-                    if (Amount > 100) return message.channel.send('Uh oh, the amout you provided too high.');
-                    const fetchChannel = await message.channel.messages.fetch({limit:Amount})
-                    fetchChannel.map(msg => msg.delete().catch(()=>{}))
-                } else if (Options === 'link') {
-                    if (!Amount) return message.channel.send('Please provide a numeric input!');
-                    if (isNaN(Amount)) return message.channel.send('Please provide a valid numeric input!');
-                    if (Amount > 100) return message.channel.send('Uh oh, the amout you provided too high.');
-                    const fetchChannel = await message.channel.messages.fetch({limit:Amount})
-                    fetchChannel.filter(msg => msg.content.includes('http') || msg.content.includes('https')).map(msg => msg.delete().catch(()=>{}))
+            if (await Command(message, "time", { prefix: ai.prefix })) {
+                if (!args[1]) {
+                    await message.channel.send(timeAgo(Date.parse(message.author.createdAt)))
                 } else {
-                    if (!Options) return message.channel.send('Please provide a numeric input!');
-                    if (isNaN(Options)) return message.channel.send('Please provide a valid numeric input!');
-                    if (Options > 100) return message.channel.send('Uh oh, the amout you provided too high.');
-                    await message.delete().catch(()=>{})
-                    await message.channel.bulkDelete(Options, true).catch(e => {}).then(messages => message.channel.send(`🚮 | Deleted ${messages.size}/${Options} messages.`)).then(lastMsg => lastMsg.delete({timeout: 2000}))
+                    const user = message.guild.members.cache.get(args[1])
+                    if (!user) return message.channel.send("Invalid ID")
+                    await message.channel.send(timeAgo(Date.parse(user.user.createdAt)))
                 }
             }
+        }
+        if (ai.config.badWords.some(words => message.content.toLowerCase().includes(words))) {
+            if (ai.developers.some(devID => message.author.id === devID)) return;
+            await message.react()
         }
     })
 
     // ############################# Guild Commands ####################################### //
+    const { InGuild } = require('./settings.json')
     client.on('message', async(message)=>{
         if (message.author.bot) return;
         if (await DMchannel(message)) return false;
-        if (message.guild.id === "771560955099611187") {
-            const { guildCommands } = require('./guilds/Discord.kyz/messages/guildCommands')
+        if (message.guild.id === InGuild.Disproberry) {
+            const { guildCommands } = require('./guilds/Disproberry/messages/guildCommands')
             await guildCommands(client, message, ai.prefix)
         }
     })
+    
     // ############################# Raw Event ####################################### //
     client.on('raw', async packet => {
         try {
@@ -199,13 +158,10 @@ async function Index() {
         )
         client.user.setActivity(`${client.users.cache.size.toString()} users with ${ai.prefix}`, {
             type: 'LISTENING',
-        }).then(function() { 
-            Activity();
-            readyLog('811756217453117461');
-        })
+        }).then(LoopStatus())
     });
 
-    function Activity(){
+    function LoopStatus(){
         setInterval(() => {
             client.user.setActivity(`${client.users.cache.size.toString()} users with ${ai.prefix}`, {
                 type: 'LISTENING',
@@ -213,51 +169,14 @@ async function Index() {
         }, 600000);
     }
 
-    async function readyLog(channelLogs) {
-        const now = new Date(Date.now()).toLocaleTimeString()
-        const channel = client.channels.cache.get(channelLogs)
-        const embed = new MessageEmbed()
-        .setColor('#70f7c1')
-        .setAuthor(`${client.user.username}`, `${client.user.displayAvatarURL()}`)
-        .setDescription(`System has been restarted at \`${now}\`.\nPossible causes:\n> ・ Dyno restarted\n> ・ Development process`)
-        await channel.send(embed).catch(()=>{})
-    }
-
-    async function reconnect(channelLogs) {
-        const now = new Date(Date.now()).toLocaleTimeString()
-        const channel = client.channels.cache.get(channelLogs)
-        const embed = new MessageEmbed()
-        .setColor("BLUE")
-        .setAuthor(`${client.user.username}`, `${client.user.displayAvatarURL()}`)
-        .setDescription(` ${client.user.username} :  \`${now}\`\n> Reconnecting.`)
-        await channel.send(embed).catch(()=>{})
-    }
-    async function disconnect(channelLogs) {
-        const now = new Date(Date.now()).toLocaleTimeString()
-        const channel = client.channels.cache.get(channelLogs)
-        const embed = new MessageEmbed()
-        .setColor("RED")
-        .setAuthor(`${client.user.username}`, `${client.user.displayAvatarURL()}`)
-        .setDescription(` ${client.user.username} :  \`${now}\`\n> Diconnected.`)
-        await channel.send(embed).catch(()=>{})
-    }
-    async function resume(channelLogs) {
-        const now = new Date(Date.now()).toLocaleTimeString()
-        const channel = client.channels.cache.get(channelLogs)
-        const embed = new MessageEmbed()
-        .setColor("PURPLE")
-        .setAuthor(`${client.user.username}`, `${client.user.displayAvatarURL()}`)
-        .setDescription(` ${client.user.username} :  \`${now}\`\n> Resume.`)
-        await channel.send(embed).catch(()=>{})
-    }
-
     // ################################################################################## //
     // Handle shard error
-    client.on('shardError', m =>                    { /**Console(`Shard Error: ${redBright(`${m}`)}`)*/ console.clear(); })
-    client.on('shardReconnecting', m =>             { /**Console(`Shard ${blueBright(`${m}`)}: Reconnecting`)*/ reconnect('811756217453117461') })
-    client.on('shardDisconnect', m =>               { /**Console(`Shard ${blueBright(`${m}`)}: Disconnected`)*/ disconnect('811756217453117461') })
-    client.on('shardReady', m =>                    { /**Console(`Shard ${greenBright(`${m}`)}: Ready`)*/ })
-    client.on('shardResume', m =>                   { /**Console(`Shard ${yellowBright(`${m}`)}: Resume`)*/ resume('811756217453117461') })
+    const now = new Date(Date.now()).toLocaleTimeString()
+    client.on('shardError', m =>                    { Console(`Shard Error: ${redBright(`${m}`)}`) })
+    client.on('shardReconnecting', m =>             { Console(`Shard ${blueBright(`${m}`)}: Reconnecting at ${now}`) })
+    client.on('shardDisconnect', m =>               { Console(`Shard ${blueBright(`${m}`)}: Disconnected at ${now}`) })
+    client.on('shardReady', m =>                    { Console(`Shard ${greenBright(`${m}`)}: Ready at ${now}`) })
+    client.on('shardResume', m =>                   { Console(`Shard ${yellowBright(`${m}`)}: Resume at ${now}`) })
     
     // Warning and Error
     client.on('warn', w =>                          { Console('Warn', `Warning: ${redBright(`${w}`)}`) })
