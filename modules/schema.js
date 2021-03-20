edf7bdd2d3b7da32fa1b394c3b98971f2dfc92b5
Axvia <65358975+Axvia@users.noreply.github.com>
2021-03-20 16:28:16 +0800

    Update schema.js

--- modules/schema.js
+++ modules/schema.js
@@ -5,7 +5,7 @@ const { redBright, yellowBright } = require('chalk')
 async function MongoDB() {
     console.log(yellowBright('Connecting to MongoDB ...'));
     mongoose.connect(MongodbSRV, { useNewUrlParser: true, useUnifiedTopology: true }).catch(err => console.log(`Info - Error: Failed to connect MongoDB [Error :: ${err}]`));
-    mongoose.connection.on('error', function (err) { console.log('MavyDB: Failed to connect to MongoDB. [Disconnected : True]'); mongoose.disconnect().catch(()=>{}); });
+    mongoose.connection.on('error', function (err) { console.log('Info: Failed to connect to MongoDB. [Disconnected : True]'); mongoose.disconnect().catch(()=>{}); });
     mongoose.connection.once('open', function (d) { console.log("\x1b[32mInfo:\x1b[0m connected to \x1b[31m" + mongoose.connection.host + " \x1b[0m"); })
 } MongoDB().catch(err => {console.log(`"MongoDB - Error: ${redBright(err)}`)})
 
